b2fd0afdcb3025760cd78a9efc9d957cf9b1e75f
ywu <wuyichen1987@gmail.com>
2019-08-17 20:29:55 +0100

    second commit

--- main.go
+++ main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
-func main(){
-	fmt.Printf("hello world, Yichen \n")
+var (
+	python, c = true, false
+	cplus     = "Yes"
+)
+
+func main() {
+	const Key = "this is a const value \n"
+
+	fmt.Printf(Key)
+
+	fmt.Printf("hello，世界 \n")
+	fmt.Println("my faviour time is :", rand.Intn(10))
+	fmt.Println(add(1, 2))
+	wordA, wordB := swap("hello", "world")
+	fmt.Println(wordA, wordB)
+	fmt.Println(split(16))
+
+	var java string = "no"
+
+	csharp := "yes"
+	fmt.Println(python, c, cplus, java, csharp)
+
+	Conversion()
+
+	CheckType()
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+// return Multi results
+func swap(wordA, wordB string) (string, string) {
+	return wordB, wordA
+}
+
+//named return value
+func split(sum int) (x, y int) {
+	x = sum - 4
+	y = sum * 16
+	return
+}
+
+//type conversion
+func Conversion() {
+	x, y := 1, 2
+	f := math.Sqrt(float64(x*x + y*y))
+	u := uint(f)
+
+	fmt.Printf("%v, %v\n", f, u)
+}
+
+//check type
+func CheckType() {
+	num := 1
+	num2 := 1.1
+
+	fmt.Printf("the data type is %T,%T", num, num2)
 }
